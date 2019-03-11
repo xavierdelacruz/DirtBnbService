7377c1d422f7469245e3c3205679a40ccbef1a39
Quintin <q.muncaster@gmail.com>
2019-03-10 20:46:24 -0700

    added review, reservation

---
+++ DirtBnBWebAPI/DirtBnBWebAPI/PersistenceServices/ReservationPersistenceService.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using DirtBnBWebAPI.Models;
+using MySql.Data.MySqlClient;
+
+namespace DirtBnBWebAPI.PersistenceServices
+{
+    public class ReservationPersistenceService
+    {
+        private readonly string PARENT_TABLE = "reservations";
+        private readonly string CHILD_TABLE = "reservations_fd_reservation_period";
+
+        private MySqlConnection sqlConnection;
+
+        public ReservationPersistenceService()
+        {
+            sqlConnection = SqlServerConnectionManager.Instance.GetSqlConnection();
+        }
+
+        // GET Reservations Call
+        public List<Reservation> GetReservations()
+        {
+            MySqlDataReader mySQLReader = null;
+            List<Reservation> reservations = new List<Reservation>();
+
+            string slqCommandString = "SELECT * FROM " + PARENT_TABLE;
+            MySqlCommand sqlCommand = new MySqlCommand(slqCommandString, sqlConnection);
+            try
+            {
+                mySQLReader = sqlCommand.ExecuteReader();
+
+                while (mySQLReader.Read())
+                {
+                    Reservation reservation = new Reservation
+                    {
+                        reservationID = mySQLReader.GetInt32(0),
+                        guestUserID = mySQLReader.GetInt32(1),
+                        accommodationID = mySQLReader.GetInt32(2),
+                        paymentID = mySQLReader.GetInt32(3),
+                        startDateTime = mySQLReader.GetDateTime(4),
+                        endDateTime = mySQLReader.GetDateTime(5)
+                        // TODO: get reservation length from fd table
+                        //reservationLength = mySQLReader.GetInt32(0)
+                    };
+                    reservations.Add(reservation);
+                }
+                mySQLReader.Close();
+                return reservations;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a GET Reservation call in ReservationPersistenceService: " + ex);
+                return null;
+            }
+        }
+
+        // GET Reservation Call
+        public Reservation GetReservation(long id)
+        {
+
+            MySqlDataReader mySQLReader = null;
+
+            string slqCommandString = "SELECT * FROM " + PARENT_TABLE + " WHERE ReservationID = " + id.ToString();
+
+            try
+            {
+                MySqlCommand sqlCommand = new MySqlCommand(slqCommandString, sqlConnection);
+                mySQLReader = sqlCommand.ExecuteReader();
+
+                if (mySQLReader.Read())
+                {
+                    Reservation reservation = new Reservation
+                    {
+                        reservationID = mySQLReader.GetInt32(0),
+                        guestUserID = mySQLReader.GetInt32(1),
+                        accommodationID = mySQLReader.GetInt32(2),
+                        paymentID = mySQLReader.GetInt32(3),
+                        startDateTime = mySQLReader.GetDateTime(4),
+                        endDateTime = mySQLReader.GetDateTime(5)
+                        // TODO: get reservation length from fd table
+                        //reservationLength = mySQLReader.GetInt32(0)
+                    };
+                    mySQLReader.Close();
+                    return reservation;
+                }
+                mySQLReader.Close();
+                return null;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a GET Reservation call in ReservationPersistenceService: " + ex);
+                return null;
+            }
+        }
+
+        // POST Reservation Call
+        public long SaveReservation(Reservation reservation)
+        {
+            /// Create child tuple if doesn't exist already
+            string childSqlCommandStringPeriod = "INSERT INTO " + CHILD_TABLE + " VALUES ('"
+                + reservation.startDateTime + "','"
+                + reservation.endDateTime + "','"
+                // TODO: add logic to calculate reservation length rather than requiring it be specified
+                + reservation.reservationLength + "')";
+
+            string sqlCommandString = "INSERT INTO " + PARENT_TABLE + " VALUES ("
+                + reservation.reservationID + ",'"
+                + reservation.guestUserID + "','"
+                + reservation.accommodationID + "','"
+                + reservation.paymentID + "','"
+                + reservation.startDateTime + "','"
+                + reservation.endDateTime + "')";
+
+            MySqlCommand childSqlCommandPeriod = new MySqlCommand(childSqlCommandStringPeriod, sqlConnection);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlCommandString, sqlConnection);
+            try
+            {
+                childSqlCommandPeriod.ExecuteNonQuery(); // TODO: maybe catch and suppress this if already in db
+                sqlCommand.ExecuteNonQuery();
+                long id = sqlCommand.LastInsertedId;
+                return id;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a POST Reservation call in ReservationPersistenceService: " + ex);
+                return -1;
+            }
+        }
+
+        // DELETE Reservation
+        public bool DeleteReservation(long id)
+        {
+            MySqlDataReader mySQLReader = null;
+            string slqCommandString = "SELECT * FROM " + PARENT_TABLE + " WHERE ReservationID = " + id.ToString();
+
+            try
+            {
+                MySqlCommand sqlCommand = new MySqlCommand(slqCommandString, sqlConnection);
+                mySQLReader = sqlCommand.ExecuteReader();
+
+                if (mySQLReader.Read())
+                {
+                    mySQLReader.Close();
+                    string slqDeleteCommandString = "DELETE FROM " + PARENT_TABLE + " WHERE ReservationID = " + id.ToString();
+                    MySqlCommand sqlDeleteCommand = new MySqlCommand(slqDeleteCommandString, sqlConnection);
+                    sqlDeleteCommand.ExecuteNonQuery();
+                    return true;
+                }
+                mySQLReader.Close();
+                return false;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a DELETE Reservation call in ReservationPersistenceService: " + ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ DirtBnBWebAPI/DirtBnBWebAPI/PersistenceServices/ReviewPersistenceService.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using DirtBnBWebAPI.Models;
+using MySql.Data.MySqlClient;
+
+namespace DirtBnBWebAPI.PersistenceServices
+{
+    public class ReviewPersistenceService
+    {
+        private MySqlConnection sqlConnection;
+
+        public ReviewPersistenceService()
+        {
+            sqlConnection = SqlServerConnectionManager.Instance.GetSqlConnection();
+        }
+
+        // GET Reviews Call
+        public List<Review> GetReviews()
+        {
+            MySqlDataReader mySQLReader = null;
+            List<Review> Reviews = new List<Review>();
+
+            string slqCommandString = "SELECT * FROM Reviews";
+            MySqlCommand sqlCommand = new MySqlCommand(slqCommandString, sqlConnection);
+            try
+            {
+                mySQLReader = sqlCommand.ExecuteReader();
+
+                while (mySQLReader.Read())
+                {
+                    Review Review = new Review();
+                    Review.reviewID = mySQLReader.GetInt32(0);
+                    Review.accommodationID = mySQLReader.GetInt32(1);
+                    Review.reservationID = mySQLReader.GetInt32(2);
+                    Review.content = mySQLReader.GetString(3);
+                    Review.rating = mySQLReader.GetInt32(4);
+                    Reviews.Add(Review);
+                }
+                mySQLReader.Close();
+                return Reviews;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a GET Reviews call in ReviewPersistenceService(GetReviews): " + ex);
+                return null;
+            }
+        }
+
+        // GET Review Call
+        public Review GetReview(long id)
+        {
+            Review Review = new Review();
+            MySqlDataReader mySQLReader = null;
+
+            string slqCommandString = "SELECT * FROM Reviews WHERE ReviewID = " + id.ToString();
+
+            try
+            {
+                MySqlCommand sqlCommand = new MySqlCommand(slqCommandString, sqlConnection);
+                mySQLReader = sqlCommand.ExecuteReader();
+
+                if (mySQLReader.Read())
+                {
+                    Review.reviewID = mySQLReader.GetInt32(0);
+                    Review.accommodationID = mySQLReader.GetInt32(1);
+                    Review.reservationID = mySQLReader.GetInt32(2);
+                    Review.content = mySQLReader.GetString(3);
+                    Review.rating = mySQLReader.GetInt32(4);
+
+                    mySQLReader.Close();
+                    return Review;
+                }
+                mySQLReader.Close();
+                return null;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a GET Review call in ReviewPersistenceService (GetReview): " + ex);
+                return null;
+            }
+        }
+
+        // POST Review
+        public long SaveReview(Review review)
+        {
+            string sqlCommandString = "INSERT INTO Reviews VALUES(" + review.reviewID + "," +
+                review.accommodationID + "," + review.reservationID + ",'" + review.content + "'," + review.rating + ")";
+     
+            MySqlCommand sqlCommand = new MySqlCommand(sqlCommandString, sqlConnection);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+                long id = sqlCommand.LastInsertedId;
+                return id;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Found an error when performing a POST Review call in ReviewPersistenceService (SaveReview): " + ex);
+                return -1;
+            }
+        }
+
+        // DELETE Review
+        // Commented out as we decided not to allow deleting reviews
+        //public bool DeleteReview(long id)
+        //{
+        //    string sqlCommandString = "SELECT * FROM Reviews WHERE ReviewID = " + id.ToString();
+        //    MySqlCommand sqlCommand = new MySqlCommand(sqlCommandString, sqlConnection);
+        //    try
+        //    {
+        //        sqlCommand.ExecuteNonQuery();
+        //        return true;
+        //    }
+        //    catch (MySqlException ex)
+        //    {
+        //        Console.WriteLine("Found an error when performing a DELETE Review call in ReviewPersistenceService: " + ex);
+        //        return false;
+        //    }
+        //}
+    }
+}
