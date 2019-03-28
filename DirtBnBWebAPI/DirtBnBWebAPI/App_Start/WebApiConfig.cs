bae38be810f30ea44593a154c61ca43c5d0d7bfd
Quintin Muncaster <q.muncaster@gmail.com>
2019-03-27 18:30:46 -0700

    added CORS support to host

--- DirtBnBWebAPI/DirtBnBWebAPI/App_Start/WebApiConfig.cs
+++ DirtBnBWebAPI/DirtBnBWebAPI/App_Start/WebApiConfig.cs
@@ -10,7 +10,8 @@ namespace DirtBnBWebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
